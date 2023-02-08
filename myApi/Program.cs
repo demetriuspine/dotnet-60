3aefc74af7d4285f24bdf9456e46d4c25701a82d
lucas <mr.pine97@gmail.com>
2023-02-08 13:52:12 -0300

    adiciona parametros e retorna status nominalmente

--- myApi/Program.cs
+++ myApi/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return Results.Ok("Hello World!!"); // retorna status 200
+});
+
+app.MapGet("/say-hello/{name}", (string name) =>
+{
+    return Results.Ok($"Hello {name}");
+});
 
 app.Run();

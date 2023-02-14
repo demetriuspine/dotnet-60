4e28d6adaef127ebefefdaede5749a1cc1cacc96
lucas <mr.pine97@gmail.com>
2023-02-14 14:08:43 -0300

    cria uma nova migration

---
+++ myApi/Migrations/20230214162655_AddDefaultCreatedAt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+            name: "CreatedAt",
+            table: "Todos",
+            type: "TEXT",
+            nullable: false,
+            defaultValueSql: "CURRENT_TIMESTAMP",
+            oldClrType: typeof(DateTime),
+            oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

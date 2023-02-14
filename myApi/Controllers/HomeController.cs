98c8fb1c108a776f2235e73198482e8028dcca89
lucas <mr.pine97@gmail.com>
2023-02-14 13:05:16 -0300

    adiciona prefixo de rota de forma correta

--- myApi/Controllers/HomeController.cs
+++ myApi/Controllers/HomeController.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace MyApi.Controllers
 {
     [ApiController]
-    [Route("v1")] // prefixo de rota
+    [Route("v1/")] // prefixo de rota
     public class HomeController : ControllerBase
     {
         [HttpGet]
-        [Route("/")] // mesma coisa que [HttpGet("/")]
+        [Route("")] // mesma coisa que [HttpGet("/")], caso o prefixo de rota esteja em uso, não pode usar o 
         public string Get() // também chamada de Action
         {
-            return "Hello World";
+            return "Hello World !";
         }
     }
 }

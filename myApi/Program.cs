1b1d801971f80f3eda55d3441b0ca8750e7c30fe
lucas <mr.pine97@gmail.com>
2023-02-14 13:05:29 -0300

    configurando as routes

--- myApi/Program.cs
+++ myApi/Program.cs
@@ -1,19 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(); // adiciona suporte a controllers
+
 var app = builder.Build();
 
-app.MapGet("/", () =>
-{
-    return Results.Ok("Hello World!!"); // retorna status 200
-});
+// app.MapGet("/", () =>
+// {
+//     return Results.Ok("Hello World!!"); // retorna status 200
+// });
 
-app.MapGet("/say-hello/{name}", (string name) =>
-{
-    return Results.Ok($"Hello {name}");
-});
+// app.MapGet("/say-hello/{name}", (string name) =>
+// {
+//     return Results.Ok($"Hello {name}");
+// });
 
-app.MapPost("/", (UserExample user) => // o asp.net ja faz a serialização e deserialização do json enviado pelo body ...
-{
-    return Results.Ok(user); //e do objeto enviado
-});
+// app.MapPost("/", (UserExample user) => // o asp.net ja faz a serialização e deserialização do json enviado pelo body ...
+// {
+//     return Results.Ok(user); //e do objeto enviado
+// });
+
+
+app.MapControllers(); // busca todos os controllers dentrod o arquivo
+
+// app.UsePathBase("/v1"); // mesmo que [Route("v1")] no HomeController
 
 app.Run();

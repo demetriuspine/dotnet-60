4b395bd2746ce879a3ff9da9b48a1b5603d634d7
lucas.pine <lucas.pine@sorteonline.com.br>
2023-03-27 14:17:38 -0300

    get categories

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("v1")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")] //localhost:PORT/v1/categories
+        public IActionResult Get([FromServices]BlogDataContext ctx)
+        {
+            var categories = ctx.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}

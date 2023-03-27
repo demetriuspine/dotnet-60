96bd732fc6e9f1a1ecc110e56c5858d046af0ce3
lucas.pine <lucas.pine@sorteonline.com.br>
2023-03-27 14:17:19 -0300

    adiciona um healthcheck

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")] // health check, para ver se a api está online
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}

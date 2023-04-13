57e0b5ea82d348e3190a103e992e1b1233a3e4d1
lucas.pine <lucas.pine@sorteonline.com.br>
2023-04-13 11:07:55 -0300

    crud async

--- Blog/Controllers/CategoryController.cs
+++ Blog/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +15,60 @@ namespace Blog.Controllers
             var categories = await ctx.Categories.ToListAsync();
             return Ok(categories);
         }
+
+        [HttpGet("categories/{id:int}")] //localhost:PORT/v1/categories/1
+        public async Task<IActionResult> GetByIdAsync([FromServices] BlogDataContext ctx, [FromRoute] int id)
+        {
+            var category = await ctx.Categories.FirstOrDefaultAsync(x=>x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost("categories")] //localhost:PORT/v1/categories
+        public async Task<IActionResult> PostAsync([FromServices] BlogDataContext ctx, [FromBody] Category model)
+        {
+            await ctx.Categories.AddAsync(model);
+
+            await ctx.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}", model);
+        }
+
+        [HttpPut("categories/{id:int}")] //localhost:PORT/v1/categories/1
+        public async Task<IActionResult> PutAsync([FromServices] BlogDataContext ctx, [FromRoute] int id, [FromBody] Category model)
+        {
+            var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            ctx.Categories.Update(category);
+
+            await ctx.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("categories/{id:int}")] //localhost:PORT/v1/categories/1
+        public async Task<IActionResult> DeleteAsync([FromServices] BlogDataContext ctx, [FromRoute] int id)
+        {
+            var category = await ctx.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            ctx.Categories.Remove(category);
+
+            await ctx.SaveChangesAsync();
+
+            return Ok(category);
+        }
+
     }
 }

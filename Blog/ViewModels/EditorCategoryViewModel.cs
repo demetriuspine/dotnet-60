2765ea201aac8f1ee4f533793d100d8c31f0b4d1
lucas.pine <lucas.pine@sorteonline.com.br>
2023-05-04 13:39:21 -0300

    refatora viewmodel para ser usada no put

--- Blog/ViewModels/EditorCategoryViewModel.cs
+++ Blog/ViewModels/EditorCategoryViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Blog.ViewModels
 {
-    public class CreateCategoryViewModel // apenas name e slug serão mandados plo postman/frontend
+    public class EditorCategoryViewModel // apenas name e slug serão mandados plo postman/frontend
     {
         public string Name { get; set; }
         public string Slug { get; set; }

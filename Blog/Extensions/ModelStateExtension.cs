0178e0f0adc60c68a955f717f25eed45157384cb
lucas.pine <lucas.pine@sorteonline.com.br>
2023-05-15 14:57:22 -0300

    adiciona extensão de ModelState

---
+++ Blog/Extensions/ModelStateExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState) // o this faz com que GetErrors() seja acessado no ModelState
+        {
+            var result = new List<string>();
+            foreach (var item in modelState.Values)
+            {
+                //foreach (var error in item.Errors)
+                //{
+                //    result.Add(error.ErrorMessage);
+                //}
+
+                result.AddRange(item.Errors.Select(error => error.ErrorMessage)); // equivalente ao código comentado de cima
+            }
+
+            return result;
+        }
+    }
+}
